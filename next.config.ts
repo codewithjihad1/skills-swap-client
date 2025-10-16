1ce02a174aa743dd7bbb74fb8792646804b46dcf
ihsanuddin637 <ihsanuddin.dev@gmail.com>
2025-10-16 15:14:11 +0600

    added courses routes

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        domains: ["images.unsplash.com", "i.ibb.co", "i.ibb.co.com"],
-        // Or use the newer remotePatterns (recommended):
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.unsplash.com",
-            },
-            {
-                protocol: "https",
-                hostname: "avatars.githubusercontent.com",
-            },
-            {
-                protocol: "https",
-                hostname: "lh3.googleusercontent.com",
-            },
-            {
-                protocol: "https",
-                hostname: "i.ibb.co",
-            },
-            {
-                protocol: "https",
-                hostname: "i.ibb.co.com",
-            },
-        ],
-    },
+  images: {
+    domains: ["images.unsplash.com", "i.ibb.co", "i.ibb.co.com"],
+    // Or use the newer remotePatterns (recommended):
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co.com",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
